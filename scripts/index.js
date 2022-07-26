9c873c63c3c1f51dd2f7b92650188783cb52c986
roshanwaa <kuroshan120@gmail.com>
2022-07-26 09:20:48 +0530

    view more done

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,12 @@
-const viewMore = document.querySelector('.showContent');
-const core_img = document.getElementsByClassName('core-img');
-const visible_content = document.getElementsByClassName('visible-content');
-const invisible_content = document.getElementsByClassName('invisible-content');
-const lessBtn = document.getElementsByClassName('more-less');
+const viewMoreBtn = document.querySelector('#view_more');
+const viewCity = document.getElementById('view-more-city-cards');
 
-$(viewMore).ready(function () {
-  $('.invisible-content').hide();
-  $(viewMore).on('click', '.more-less', function () {
-    var moreLessButton = $('.invisible-content').is(':visible')
-      ? 'View More'
-      : 'View Less';
-
-    $(this).text(moreLessButton);
-    if ($(lessBtn).text() == 'View Less') {
-      $(core_img).show(invisible_content).find(invisible_content).toggle();
-    }
-    // $(core_img).show(invisible_content).find(visible_content).toggle();
-    if ($(lessBtn).text() == 'View More') {
-      $(core_img).hide(core_img).find(visible_content).toggle();
-    }
-  });
+viewMoreBtn.addEventListener('click', () => {
+  if (viewMoreBtn.innerText == 'View More') {
+    viewCity.style.display = 'block';
+    viewMoreBtn.innerText = 'View Less';
+  } else {
+    viewCity.style.display = 'none';
+    viewMoreBtn.innerText = 'View More';
+  }
 });
